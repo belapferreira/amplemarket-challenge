4d19d696e068c99bf2dd4d59738dfa6e2af11298
belapferreira <isabelapenhaferreira@gmail.com>
2025-03-22 09:57:58 -0300

    feat: add error handling in the API route

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -11,50 +11,56 @@ type Post = {
 };
 
 export async function GET() {
-  const posts = await fetch(URL)
-    .then((response) => {
-      switch (response.status) {
-        // Status "ok"
-        case 200:
-          return response.text();
-        case 404:
-          throw response;
-      }
-    })
-    .catch((response) => {
-      console.log(response.statusText);
+  try {
+    const posts = await fetch(URL)
+      .then((response) => {
+        switch (response.status) {
+          // Status "ok"
+          case 200:
+            return response.text();
+          case 404:
+            throw response;
+        }
+      })
+      .catch((response) => {
+        console.log(response.statusText);
+      });
+
+    const parsedPosts = cheerio.load(posts || '');
+
+    const postsList = [] as Post[];
+
+    parsedPosts('.athing').each(function (i) {
+      postsList[i] = {
+        position: Number(
+          parsedPosts(this).find('span.rank').text().trim().replace('.', '')
+        ),
+        title: parsedPosts(this).find('span.titleline a').text().trim(),
+        link: parsedPosts(this).find('span.titleline a').attr('href'),
+        points: 0,
+      };
     });
 
-  const parsedPosts = cheerio.load(posts || '');
+    parsedPosts('.athing + tr').each(function (i) {
+      postsList[i] = {
+        ...postsList[i],
+        points: Number(
+          parsedPosts(this)
+            .find('span.score')
+            .text()
+            .trim()
+            .replace(' points', '')
+        ),
+      };
+    });
 
-  const postsList = [] as Post[];
-
-  parsedPosts('.athing').each(function (i) {
-    postsList[i] = {
-      position: Number(
-        parsedPosts(this).find('span.rank').text().trim().replace('.', '')
-      ),
-      title: parsedPosts(this).find('span.titleline a').text().trim(),
-      link: parsedPosts(this).find('span.titleline a').attr('href'),
-      points: 0,
-    };
-  });
-
-  parsedPosts('.athing + tr').each(function (i) {
-    postsList[i] = {
-      ...postsList[i],
-      points: Number(
-        parsedPosts(this)
-          .find('span.score')
-          .text()
-          .trim()
-          .replace(' points', '')
-      ),
-    };
-  });
-
-  return NextResponse.json({
-    posts: postsList,
-    now: Date.now(),
-  });
+    return NextResponse.json({
+      posts: postsList,
+      now: Date.now(),
+    });
+  } catch {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
 }
